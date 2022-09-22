f830077e8b3d6ce808edffd66799f7bce415c5fe
Favisoki <adedokunisaac783@gmail.com>
2022-09-22 11:46:27 +0100

    Add model for payment

---
+++ src/models/payment_detail.py
@@ -0,0 +1,20 @@
+"""
+Define the Payment Detail model
+"""
+from . import db
+from .abc import BaseModel, MetaBaseModel
+from datetime import datetime
+
+
+class PaymentDetail(db.Model, BaseModel, metaclass=MetaBaseModel):
+    """ The Payment Details model """
+
+    __tablename__ = "payment-details"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, nullable=False, unique=True)
+    amount = db.Column(db.Integer)
+    status = db.Column(db.String(10))
+    payment_initiated = db.Column(db.DateTime(), default=datetime.utcnow)
+    paymentmethod_id = db.Column(db.Integer)
+
