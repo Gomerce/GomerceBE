716035713aea3750a4a5d9cb20fd0ffb555215f9
Abednego-py <abednegoaginam@gmail.com>
2022-10-07 20:27:15 +0100

    changes to orders-model

--- src/models/order.py
+++ src/models/order.py
@@ -5,32 +5,27 @@ Define the Orders model
 from . import db
 from .abc import BaseModel, MetaBaseModel
 from datetime import datetime
-from enum import Enum, unique
-from sqlalchemy.dialects.postgresql import ENUM as pgEnum
 
-@unique
-class Status(Enum):
-    Pending = 'pending'
-    Processing = 'processing'
-    Completed = 'completed'
 
-class Order(db.Model, BaseModel, metaclass=MetaBaseModel):
+class Orders(db.Model, BaseModel, metaclass=MetaBaseModel):
     """ The Orders model """
 
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    modified_at = db.Column(db.DateTime, default=datetime.utcnow)
-    total_order_cost = db.Column(db.Float, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    total_cost = db.Column(db.Float, nullable=False)
     tax = db.Column(db.Float, nullable=False)
     delivery_status = db.Column(db.Boolean, nullable=False, default=False)
-    delivery_date = db.Column(db.DateTime, nullable=False)
-    status = db.Column(pgEnum(Status), unique=False, nullable=False)
+    delivered_at = db.Column(db.DateTime, nullable=False)
+    status_id = db.Column(db.Integer, db.ForeignKey("status.id"), nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    shipping_address_id = db.Column(db.Integer, db.ForeignKey("shipping_addresses.id"), nullable=False)
     seller_id = db.Column(db.Integer, db.ForeignKey("sellers.id"), nullable=False)
     coupon_id = db.Column(db.Integer, db.ForeignKey("coupons.id"), nullable=False)
-    payment_details = db.relationship("paymentDetails", backref="orders", lazy=True)
-    order_details =  db.relationship("orderdetails", backref="orders", lazy=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey("cart.id"), nullable=False)
+    payment_details = db.relationship("payment_details", backref="orders", lazy=True)
+    order_details =  db.relationship("order_details", backref="orders", lazy=True)
 
 
