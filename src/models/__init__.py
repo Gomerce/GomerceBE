b93c80866adb7416a4b137755d87de861fae176b
Abednego-py <abednegoaginam@gmail.com>
2022-10-07 20:30:42 +0100

    added imports in init file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,4 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .customer import Customer
+from .verification_token import VerificationToken
+from .product import Product
+from .store import Store
+from .coupon import Coupon
+from .payment_detail import PaymentDetail
+from .shipping_address import ShippingAddress
+from .payment_method import PaymentMethod
 from .order import Orders
