e6acc017ba784d772a72b88f86d6838639413c9b
Hassan Yahya A <hassanyahya400@gmail.com>
2022-10-08 10:33:58 +0100

    add status model

--- src/models/status.py
+++ src/models/status.py
@@ -7,7 +7,6 @@ from .abc import BaseModel, MetaBaseModel
 from sqlalchemy import ForeignKey
 
 
-
 class Status(db.Model, BaseModel, metaclass=MetaBaseModel):
      """"
      The Status Model
@@ -17,7 +16,6 @@ class Status(db.Model, BaseModel, metaclass=MetaBaseModel):
 
      id = db.Column(db.Integer, primary_key=True)
      status = db.Column(db.String(100), nullable=False)
-
 
      #Foreign Key
      order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
