958aabbb4d8868514859904a975687bf0d75bc14
aniekandan <aniekandan05@gmail.com>
2022-11-07 00:24:35 +0100

    removed personal migrations

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,15 +1,14 @@
 from .index import IndexResource
 from .customer import CustomerResource
 from .auth import AuthResource
-<<<<<<< HEAD
+
 from .payment_detail import PaymentDetailResource
 from .payment_method import PaymentMethodResource
 from .order import OrderResource
 from .order_detail import OrderDetailResource
 
-=======
 from .categories import CategoriesResource
 from .products import ProductResource
 from .sellers import SellerResource
 from .google_auth import GoogleResource
->>>>>>> 3ceca22a53e5284108de638a3ee4da27ad8a9ee9
+
