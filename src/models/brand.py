6b1466ce256a51d168ad027f72e840b7c0c42bf7
Oluwatobi Odekunle <ooluwatobialao@gmail.com>
2023-02-03 18:45:37 +0100

    feat: create brand model

--- src/models/brand.py
+++ src/models/brand.py
@@ -7,17 +7,19 @@ from . import db
 from .abc import BaseModel, MetaBaseModel
 from datetime import datetime
 
-class ProductCategory(db.Model, BaseModel, metaclass=MetaBaseModel):
-    """ The Product Category model """
+class Brand(db.Model, BaseModel, metaclass=MetaBaseModel):
+    """ The Brand model """
 
-    __tablename__ = "product_categories"
+    __tablename__ = "brands"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    sku = db.Column(db.String(300), nullable=False)
+    image = db.Column(db.String(100))
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
+    #Foregn Keys
+    product_categories_id = db.Column(db.Integer, db.ForeignKey('product_categories.id'), nullable=False)
+
     #Relationship
-    products = db.relationship('Product', backref='product_categories', lazy=True)
-    brands = db.relationship('Brand', backref='brands', lazy=True)
+    products = db.relationship('Product', backref='brands', lazy=True)
